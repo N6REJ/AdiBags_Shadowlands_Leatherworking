d37143e59d14944c05c8611370b4988d04014851
Bear <programming@hallhome.us>
2022-10-29 20:48:51 -0500

    fix typo add namespacing

--- database.lua
+++ database.lua
@@ -1,12 +1,12 @@
 -- AdiBags_Bears_Leatherworking - Database
 -- Created by @project-author@ character is Bearesquishy - dalaran please credit whenever.
 -- Source on GitHub: https://n6rej.github.io
-
+---@type
 local addonName, addonTable = ...;
 
--- Get locals namse
+-- Get locals names
 local L = addonTable.locales;
-local Expansion = L["SHADOWLANDS"]
+local Expansion = L["Shadowlands"]
 
 -- Create addon table
 local db = {}
